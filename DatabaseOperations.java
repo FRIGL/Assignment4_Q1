1b7a8e02affbf1c648a724fda13cd2a202f2ee54
FRIGL <M4s0nm@outlook.com>
2023-11-23 16:02:41 -0500

    got deleteStudent() and updateStudentEmail() working

--- DatabaseOperations.java
+++ DatabaseOperations.java
@@ -2,7 +2,10 @@ import java.sql.Connection;
 import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.SimpleDateFormat;
 import java.util.Scanner;
 
@@ -12,6 +15,59 @@ public class DatabaseOperations {
     private final String url = "jdbc:postgresql://localhost:5432/DemoDB";
     private final String user = "postgres";
     private final String password = "password";
+
+    
+    public void getAllStudents()
+    {   
+
+        //Connection connection = DriverManager.getConnection(url, user, password);
+        
+        //Creating the Statement
+        //Statement statement = connection.createStatement();
+        //Query to retrieve records
+        //String query = "Select * from MyPlayers";
+        //Executing the query
+        //ResultSet resultSet = statement.executeQuery(query);
+        //Retrieving ResultSetMetaData object
+        //ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+
+         String SQL = "SELECT s FROM students s;";
+
+        try (Connection connection = DriverManager.getConnection(url, user, password);
+            Statement stmt = connection.createStatement()) 
+            {
+                 //Query to retrieve records
+                String query = "Select * from students";
+
+                //Executing the query
+                ResultSet resultSet = stmt.executeQuery(query);
+
+                ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+
+                System.out.println(resultSetMetaData.getColumnCount());//i think this works
+
+                System.out.println("good?");
+            }
+        catch (SQLException ex)
+            {
+                System.out.println(ex.getMessage());
+            }
+
+        /* 
+        String SQL = "SELECT s FROM students s;";
+        
+        try (Connection conn = DriverManager.getConnection(url, user, password);
+            PreparedStatement pstmt = conn.prepareStatement(SQL)) 
+            {
+                pstmt.executeUpdate();
+                System.out.println("good?");
+            }
+        catch (SQLException ex)
+            {
+                System.out.println(ex.getMessage());
+            }
+            */
+    }
 
     // Add a user
     public void addUser(String name, String email) {
@@ -68,6 +124,24 @@ public class DatabaseOperations {
         }
     }
 
+    // Update student's email based on student_id
+    public void updateStudentEmail(int student_id, String newEmail) {
+        String SQL = "UPDATE students SET email=? WHERE student_id=?";
+
+        try (Connection conn = DriverManager.getConnection(url, user, password);
+            PreparedStatement pstmt = conn.prepareStatement(SQL))
+            {
+                pstmt.setString(1, newEmail);
+                pstmt.setInt(2, student_id);
+                pstmt.executeUpdate();
+                System.out.println("User email updated!");
+            } 
+        catch(SQLException ex)
+            {
+                System.out.println(ex.getMessage());
+            }
+    }
+
     // Delete user based on name
     public void deleteUser(String name) {
         String SQL = "DELETE FROM users WHERE name=?";
@@ -85,16 +159,17 @@ public class DatabaseOperations {
     }
 
     // Delete student based on id
-    public void deleteStudent(int studen_id) {
+    public void deleteStudent(int student_id)
+    {
         String SQL = "DELETE FROM students WHERE student_id=?";
 
         try (Connection conn = DriverManager.getConnection(url, user, password);
             PreparedStatement pstmt = conn.prepareStatement(SQL))
             {
 
-                pstmt.setString(1, name);
+                pstmt.setInt(1, student_id);
                 pstmt.executeUpdate();
-                System.out.println("User deleted!");
+                System.out.println("Student deleted!");
             }
         catch(SQLException ex)
             {
@@ -102,29 +177,35 @@ public class DatabaseOperations {
             }
     }
 
-    public static void main(String[] args) {
-      DatabaseOperations dbOps = new DatabaseOperations();
-      Scanner scanner = new Scanner(System.in);
-      
-    dbOps.addStudent("John", "Doe", "john@example.com", new Date(10));
+    public static void main(String[] args) 
+    {
+        DatabaseOperations dbOps = new DatabaseOperations();
+        Scanner scanner = new Scanner(System.in);
+        
+        //dbOps.addStudent("John", "Doe", "john@example.com", new Date(10));
+        //dbOps.deleteStudent(5);
+        //dbOps.updateStudentEmail(1, "ahhh");
+        dbOps.getAllStudents();
 
-      // Add a user
-      System.out.println("Would you like to add a user? (yes/no)");
-      if (scanner.nextLine().equalsIgnoreCase("yes")) {
-          dbOps.addUser("John Doe", "john@example.com");
-      }
-  
-      // Modify user's email
-      System.out.println("Would you like to modify a user's email? (yes/no)");
-      if (scanner.nextLine().equalsIgnoreCase("yes")) {
-          dbOps.modifyUserEmail("John Doe", "john.doe@example.com");
-      }
-  
-      // Delete user
-      System.out.println("Would you like to delete a user? (yes/no)");
-      if (scanner.nextLine().equalsIgnoreCase("yes")) {
-          dbOps.deleteUser("John Doe");
-      }
-      scanner.close();
-  }
+        /* 
+        // Add a user
+        System.out.println("Would you like to add a user? (yes/no)");
+        if (scanner.nextLine().equalsIgnoreCase("yes")) {
+            dbOps.addUser("John Doe", "john@example.com");
+        }
+    
+        // Modify user's email
+        System.out.println("Would you like to modify a user's email? (yes/no)");
+        if (scanner.nextLine().equalsIgnoreCase("yes")) {
+            dbOps.modifyUserEmail("John Doe", "john.doe@example.com");
+        }
+    
+        // Delete user
+        System.out.println("Would you like to delete a user? (yes/no)");
+        if (scanner.nextLine().equalsIgnoreCase("yes")) {
+            dbOps.deleteUser("John Doe");
+        }
+        */
+        scanner.close();
+    }
 }
